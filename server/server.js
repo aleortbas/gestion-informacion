f6acc9321d20596f6c9fc9a059f1859c2ca6aba7
Alejandro Ortiz <aleortbas@gmail.com>
2023-06-18 18:45:55 -0500

    update working

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const pool = require("./dbConnection");
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
 
 app.get("/", function (req, res) {
   res.end("home");
@@ -20,7 +24,37 @@ app.get("/area", async function (req, res) {
   }
 });
 
-app.post("/anadirEmpresa", async (res, resp) => {});
+app.post("/anadirEmpresa", async (req, res) => {
+  const {
+    cod_empresa,
+    nombre_empresa,
+    cod_unidad,
+    nombre_unidad,
+    cod_area,
+    nombre_area,
+    id_subarea,
+    nombre_subarea,
+  } = req.body;
+
+  try {
+    const result = await pool
+      .request()
+      .input("cod_empresa", cod_empresa)
+      .input("nombre_empresa", nombre_empresa)
+      .input("cod_unidad", cod_unidad)
+      .input("nombre_unidad", nombre_unidad)
+      .input("cod_area", cod_area)
+      .input("nombre_area", nombre_area)
+      .input("id_subarea", id_subarea)
+      .input("nombre_subarea", nombre_subarea)
+      .query(
+        "UPDATE [dbo].[gi_empresa] SET [cod_empresa] = @cod_empresa ,[nombre_empresa] = @nombre_empresa ,[cod_unidad] = @cod_unidad ,[nombre_unidad] = @nombre_unidad ,[cod_area] = @cod_area ,[nombre_area] = @nombre_area ,[id_subarea] = @id_subarea ,[nombre_subarea] = @nombre_subarea WHERE cod_empresa = @cod_empresa"
+      );
+    console.log("UPDATE WORKING", result);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.listen(5000, function (params) {
   console.log("Server started");
