c29bf262c3b60de22d7cdf4ec6e459d9c48ceece
Alejandro Ortiz <aleortbas@gmail.com>
2023-06-21 13:41:18 -0500

    insert cliente workin

--- server/server.js
+++ server/server.js
@@ -87,6 +87,31 @@ app.post("/editarEmpresa", async (req, res) => {
   }
 });
 
+app.get("/cliente", async function (req, res) {
+  try {
+    const result = await pool.request().query("SELECT * FROM gi_cliente");
+    res.json(result.recordset);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ err: "Ocurrio un error" });
+  }
+});
+
+app.post("/anadiCliente", async function (req, res) {
+
+  const email_cliente = req.body.email_cliente;
+
+  try {
+    const result = await pool
+      .request()
+      .input("email", email_cliente)
+      .query("DECLARE @email_corporativo VARCHAR(255) DECLARE @codigo_area VARCHAR(255) DECLARE @nombre_area VARCHAR (255) DECLARE @nombre_completo VARCHAR(255) SELECT @email_corporativo=[email_corporativo],@nombre_completo=[nombre_completo], @codigo_area=[codigo_area], @nombre_area=[nombre_area] FROM [YaEncuestado].[dbo].[gi_colaboradores] WHERE email_corporativo = @email INSERT INTO [dbo].[gi_cliente] ([email_cliente] ,[nombre_cliente] ,[cod_puesto] ,[nombre_puesto]) VALUES (@email_corporativo, @nombre_completo,@codigo_area,@nombre_area)")
+    console.log("INSERT WORKINNG", result)
+  } catch (error) {
+    console.error(error)
+  }
+})
+
 app.listen(5000, function (params) {
   console.log("Server started");
 });
