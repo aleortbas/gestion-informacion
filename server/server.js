890bc65129b7e26a2d960c8a2361533502ea9792
Alejandro Ortiz <aleortbas@gmail.com>
2023-06-22 11:47:06 -0500

    cliente CRUD working

--- server/server.js
+++ server/server.js
@@ -124,6 +124,78 @@ app.delete("/eliminarCliente/:email", async function (req, res) {
   }
 })
 
+app.get("/proveedor", async function (req, res) {
+  try {
+    const result = await pool.request().query("SELECT * FROM gi_proveedor")
+    res.json(result.recordset)
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+app.post("/anadirProveedor", async function (req, res) {
+
+  const {
+    nit,
+    nombre_proveedor,
+    email_proveedor,
+    telefono_proveedor
+  } = req.body;
+
+  const abreviatura = "abreviatura"
+
+  try {
+    const result = await pool.request()
+      .input("nit", nit)
+      .input("nombre_proveedor", nombre_proveedor)
+      .input("abreviatura", abreviatura)
+      .input("email_proveedor", email_proveedor)
+      .input("telefono_proveedor", telefono_proveedor)
+      .query("INSERT INTO [dbo].[gi_proveedor] ([nit_proveedor] ,[nombre_proveedor] ,[abreviatura_proveedor] ,[email_proveedor] ,[telefono_proveedor]) VALUES(@nit, @nombre_proveedor, @abreviatura, @email_proveedor, @telefono_proveedor)")
+    alert("INSERT WORKING")
+    console.log("INSERT WORKING")
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+app.post("/editarProveedor", async function (req, res) {
+  const {
+    nit,
+    nombre_proveedor,
+    email_proveedor,
+    telefono_proveedor
+  } = req.body;
+
+  const abreviatura = "abreviatura"
+
+  try {
+    const result = await pool.request()
+      .input("nit", nit)
+      .input("nombre_proveedor", nombre_proveedor)
+      .input("abreviatura", abreviatura)
+      .input("email_proveedor", email_proveedor)
+      .input("telefono_proveedor", telefono_proveedor)
+      .query("UPDATE [dbo].[gi_proveedor] SET nit_proveedor = @nit, nombre_proveedor = @nombre_proveedor, abreviatura_proveedor = @abreviatura, email_proveedor = @email_proveedor, telefono_proveedor = @telefono_proveedor WHERE email_proveedor = @email_proveedor")
+    console.log("UPDATE WORKING")
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+app.delete("/eliminarProveedor/:email", async function (req, res) {
+  const email_proveedor = req.params.email;
+  try {
+    let res = await pool
+      .request()
+      .input("email", email_proveedor)
+      .query("DELETE FROM [dbo].[gi_proveedor] WHERE email_proveedor = @email")
+    console.log("DELTE WORKING")
+  } catch (error) {
+    console.log(error)
+  }
+})
+
 app.listen(5000, function (params) {
   console.log("Server started");
 });
