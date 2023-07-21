2463de98db8387de4ccded5df79549841ac357f0
Alejandro Ortiz <aleortbas@gmail.com>
2023-07-20 21:38:29 -0500

    empresas routes server sides

---
+++ server/routes/auth.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const pool = require("../dbConnection");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
+
+const router = express.Router();
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
+router.post('/', async (req, res) => {
+    const {
+        email,
+        password
+    } = req.body
+
+    try {
+        const result = await pool.request()
+            .input("email", email)
+            .input("password", password)
+            .query("SELECT * FROM gi_cliente WHERE email_cliente =@email AND contraseña =@password")
+        const user = { email: email };
+
+        const count = result.recordset[0];
+        //console.log(count)
+
+        if (count != null) {
+            const accessToken = generateAccesToken(user);
+            //console.log("TOKEN: ", accessToken);
+            res.json(accessToken)
+        } else {
+            res.json(null)
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+function generateAccesToken(user) {
+    return jwt.sign(user, process.env.SECRET, { expiresIn: '5m' });
+}
+
+module.exports = router;
---
+++ server/routes/empresa.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const pool = require("../dbConnection");
+const bodyParser = require("body-parser");
+
+const router = express.Router();
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
+router.get("/empresa", async function (req, res) {
+    try {
+        const result = await pool.request().query("SELECT * FROM gi_empresa");
+        res.json(result.recordset);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ err: "Ocurrio un error" });
+    }
+});
+
+router.route("/anadirEmpresa").post(async (req, res) => {
+    const {
+        cod_empresa,
+        nombre_empresa,
+        cod_unidad,
+        nombre_unidad,
+        cod_area,
+        nombre_area,
+        id_subarea,
+        nombre_subarea,
+    } = req.body;
+
+    try {
+        const result = await pool
+            .request()
+            .input("cod_empresa", cod_empresa)
+            .input("nombre_empresa", nombre_empresa)
+            .input("cod_unidad", cod_unidad)
+            .input("nombre_unidad", nombre_unidad)
+            .input("cod_area", cod_area)
+            .input("nombre_area", nombre_area)
+            .input("id_subarea", id_subarea)
+            .input("nombre_subarea", nombre_subarea)
+            .query("INSERT INTO [dbo].[gi_empresa] ([cod_empresa] ,[nombre_empresa] ,[cod_unidad] ,[nombre_unidad] ,[cod_area] ,[nombre_area] ,[id_subarea] ,[nombre_subarea]) VALUES (@cod_empresa , @nombre_empresa , @cod_unidad , @nombre_unidad , @cod_area , @nombre_area , @id_subarea , @nombre_subarea)")
+        console.log("INSERT WORKING", result);
+
+    } catch (error) {
+        console.log("NO FUNCIONO EL INSERT INTO: ", cod_area)
+    }
+})
+
+router.route("/editarEmpresa").post(async (req, res) => {
+    const {
+        cod_empresa,
+        nombre_empresa,
+        cod_unidad,
+        nombre_unidad,
+        cod_area,
+        nombre_area,
+        id_subarea,
+        nombre_subarea,
+    } = req.body;
+
+
+    try {
+        const result = await pool
+            .request()
+            .input("cod_empresa", cod_empresa)
+            .input("nombre_empresa", nombre_empresa)
+            .input("cod_unidad", cod_unidad)
+            .input("nombre_unidad", nombre_unidad)
+            .input("cod_area", cod_area)
+            .input("nombre_area", nombre_area)
+            .input("id_subarea", id_subarea)
+            .input("nombre_subarea", nombre_subarea)
+            .query(
+                "UPDATE [dbo].[gi_empresa] SET [cod_empresa] = @cod_empresa ,[nombre_empresa] = @nombre_empresa ,[cod_unidad] = @cod_unidad ,[nombre_unidad] = @nombre_unidad ,[cod_area] = @cod_area ,[nombre_area] = @nombre_area ,[nombre_subarea] = @nombre_subarea WHERE cod_empresa = @cod_empresa"
+            );
+        console.log("UPDATE WORKING", result);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+module.exports = router
